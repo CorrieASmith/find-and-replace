ddae9314f509de6ca3b16636c170b6ef84bde041
Corrie <corriesmith87@gmail.com>
2015-09-11 10:23:57 -0700

    spec quick addition

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
 describe("findReplace", function () {
-  it("will find a word in a string and replace it with another", function () {
+  it("will find all instances of a word in a string and replace it with another", function () {
     expect(findReplace("i think your face is cool", "face", "mom")).to.equal("i think your mom is cool");
+  });
+
+  it("will convert all input to lower case", function () {
+    expect(findReplace("i WAnT chEEtos", "cheetos", "fries")).to.equal("i want fries");
   });
 });
